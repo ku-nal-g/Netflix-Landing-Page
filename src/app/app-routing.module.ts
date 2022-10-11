fc453b54a5cf1091307aa2727863e8ca3f05cbd5
Kunal Gupta <kunalg02092001@gmail.com>
2022-10-11 19:20:30 +0530

    Netflix landing page

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,26 @@
+import { LoginComponent } from './components/login/login.component';
+import { HomeComponent } from './components/home/home.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
